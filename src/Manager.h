4c9c61700c1f348459e12ec608753afcec006575
klevisresuli <klevisresuli@outlook.com>
2024-11-11 16:32:24 +0100

    first changes

--- src/Manager.h
+++ src/Manager.h
@@ -6,8 +6,21 @@
 #define VDSPROJECT_MANAGER_H
 
 #include "ManagerInterface.h"
+#include <vector>
 
 namespace ClassProject {
+	typedef struct
+	{
+		BDD_ID id;
+		std::string label;
+		BDD_ID high;
+		BDD_ID low;
+		BDD_ID top_var;
+	} BDD_Var;
+
+	std::vector<BDD_Var> BDD_Var_Table;
+	
+	
 	class Manager : public ManagerInterface {
 	
 	};
