436cf20f800c1818aabe4182ca0124215b67450a
Mohamed El Habib Djamel ASLAOUI <aslaoui.mohamed@gmail.com>
2024-11-11 22:41:35 +0100

    added some basic functions

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,10 +1,11 @@
 #include "Manager.h"
 namespace ClassProject {
 
-BDD_ID ManagerInterface::createVar(const std::string &label){
+BDD_ID Manager::createVar(const std::string &label){
     BDD_Var new_var;
     last_id++;
     new_var.id = last_id;
+    
     //values of the high and low are not defined yet and depends on the label definition
 
     //new_var.label = label;
@@ -14,4 +15,22 @@ BDD_ID ManagerInterface::createVar(const std::string &label){
     //BDD_Var_Table.push_back(new_var);
     return new_var.id;
 }
+
+const BDD_ID &Manager::True(){
+    return static_cast<BDD_ID>(1);
+}
+const BDD_ID &Manager::False(){
+    return static_cast<BDD_ID>(0);
+}
+
+
+bool Manager::isConstant(BDD_ID f){
+    return ( (f==Manager::False()) || (f==Manager::True()));
+}
+bool Manager::isVariable(BDD_ID f){
+    return ( (f!=Manager::False()) && (f!=Manager::True()));
+}
+
+
+
 }
--- src/Manager.h
+++ src/Manager.h
@@ -22,14 +22,22 @@ namespace ClassProject {
 	
 	class Manager : public ManagerInterface {
 
+		BDD_ID last_id = 1;
 		std::vector<BDD_Var> BDD_Var_Table;
 		Manager(){
 			BDD_Var_Table.push_back({0, "False", 0, 0, 0});
 			BDD_Var_Table.push_back({1, "True", 1, 1, 1});
 		}
 
-	
-	};
+        BDD_ID createVar(const std::string &label);
+
+        const BDD_ID &True();
+        const BDD_ID &False();
+
+		bool isConstant(BDD_ID f);
+        bool isVariable(BDD_ID f);
+		
+    };
 }
 
 #endif
